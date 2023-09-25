6125b9dbe8026f2773a47d96d400a5ff1bdff47b
Pratibha <pratibha.gupta@publicissapient.com>
2023-09-25 16:45:34 +0530

    promise kit removed

--- SDUSampleApp/AppModules/Domain/Repository/LaunchViewRepositoryProtocol.swift
+++ SDUSampleApp/AppModules/Domain/Repository/LaunchViewRepositoryProtocol.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import PromiseKit
 
 typealias ScreenDomainResponse = ScreenModel
 
--- SDUSampleApp/AppModules/Presentation/ViewModel/PSViewModel.swift
+++ SDUSampleApp/AppModules/Presentation/ViewModel/PSViewModel.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import SwiftUI
-import PromiseKit
 
 @MainActor
 class PSViewModel: PSViewModelProtocol {
